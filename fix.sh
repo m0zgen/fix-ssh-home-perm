e6b4a35bd2cdcb4bb5cf4f1b34c0dc20ab1b1b80
/:) <g.yevgeniy.p@gmail.com>
2021-12-11 09:52:22 +0300

    + TODO's

--- fix.sh
+++ fix.sh
@@ -2,12 +2,15 @@
 # Script for fixing / correcting ~/.ssh permissions
 # Created by Yevgeniy Goncharov, https://sys-adm.in
 
+# TODO - add param username
+# TODO - add target catalog param
+
 iam=`whoami`
 
 if [[ -d ~/.ssh ]]; then
     echo "Catalog ~/.ssh found. Fixing..."
-    sudo chown -R $iam:$iam ~/.ssh/
-    sleep 1
+    chown -R $iam:$iam ~/.ssh/
+    sleep 3
     chmod u=rwx,go= ~/.ssh
     chmod -R u=rw,go= ~/.ssh/*
     chmod -R a=r,u+w ~/.ssh/*.pub
